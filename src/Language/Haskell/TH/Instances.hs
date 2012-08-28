ba8f52e34e60922dade45b2f75c4303e3b77910c
mgsloan <mgsloan@mgsloan-lappy2.(none)>
2012-08-27 21:59:23 -0700

    Fixes for GHC7.6

--- src/Language/Haskell/TH/Instances.hs
+++ src/Language/Haskell/TH/Instances.hs
@@ -18,11 +18,24 @@ module Language.Haskell.TH.Instances () where
 import Language.Haskell.TH.Syntax
 import Language.Haskell.TH.Ppr
 import Language.Haskell.TH.Lift (deriveLiftMany)
+import GHC.Word ( Word8 )
 
+
+-- Orphan Show instances
 deriving instance Show Loc
 
+
+-- Orphan Eq instances
 deriving instance Eq Loc
 deriving instance Eq Info
+
+
+-- Orphan Ord instances
+instance Ord FixityDirection where
+  (<=) InfixL _      = True
+  (<=) _      InfixR = True
+  (<=) InfixN InfixN = True
+  (<=) _      _      = False
 
 deriving instance Ord Info
 deriving instance Ord Fixity
@@ -45,26 +58,45 @@ deriving instance Ord Pat
 deriving instance Ord Lit
 
 #if MIN_VERSION_template_haskell(2,4,0)
-deriving instance Ord TyVarBndr
-deriving instance Ord Pred
-deriving instance Ord Kind
 deriving instance Ord FamFlavour
-deriving instance Ord InlineSpec
 deriving instance Ord Pragma
-#endif /* MIN_VERSION_template_haskell(2,4,0) */
+deriving instance Ord Pred
+deriving instance Ord TyVarBndr
+#endif
 
-instance Ord FixityDirection where
-  (<=) InfixL _      = True
-  (<=) _      InfixR = True
-  (<=) InfixN InfixN = True
-  (<=) _      _      = False
+#if MIN_VERSION_template_haskell(2,4,0) && !(MIN_VERSION_template_haskell(2,8,0))
+deriving instance Ord InlineSpec
+deriving instance Ord Kind
+#endif
 
+{- TODO: test?
+#if MIN_VERSION_template_haskell(2,5,0) && !(MIN_VERSION_template_haskell(2,7,0))
+deriving instance ClassInstance
+#endif
+-}
+
+#if MIN_VERSION_template_haskell(2,8,0)
+deriving instance Ord Inline
+deriving instance Ord Phases
+deriving instance Ord RuleBndr
+deriving instance Ord RuleMatch
+deriving instance Ord TyLit
+#endif
+
+
+-- Orphan Ppr instances
 -- TODO: make this better
 instance Ppr Loc where
   ppr = showtextl . show
 
 instance Ppr Lit where
   ppr l = ppr (LitE l)
+
+-- Orphan Lift instances (for when your TH generates TH!)
+-- TODO: Shouldn't this have explicit type signatures?
+--       This follows the pattern of the Lift instances for Int / Integer.
+instance Lift Word8 where
+  lift w = [e| fromIntegral $(lift (fromIntegral w :: Int)) |]
 
 $(deriveLiftMany [ ''Body
                  , ''Callconv
@@ -86,16 +118,29 @@ $(deriveLiftMany [ ''Body
                  , ''Stmt
                  , ''Strict
                  , ''Type
+
 #if MIN_VERSION_template_haskell(2,4,0)
                  , ''FamFlavour
-                 , ''InlineSpec
-                 , ''Kind
                  , ''Pragma
                  , ''Pred
                  , ''TyVarBndr
+#endif
+
 #if MIN_VERSION_template_haskell(2,5,0) && !(MIN_VERSION_template_haskell(2,7,0))
                  , ''ClassInstance
-#endif /* MIN_VERSION_template_haskell(2,5,0) && !(MIN_VERSION_template_haskell(2,7,0)) */
-#endif /* MIN_VERSION_template_haskell(2,4,0) */
+#endif
+
+#if MIN_VERSION_template_haskell(2,4,0) && !(MIN_VERSION_template_haskell(2,8,0))
+                 , ''InlineSpec
+                 , ''Kind
+#endif
+
+#if MIN_VERSION_template_haskell(2,8,0)
+                 , ''Inline
+                 , ''Phases 
+                 , ''RuleBndr
+                 , ''RuleMatch
+                 , ''TyLit
+#endif
                  ])
 
