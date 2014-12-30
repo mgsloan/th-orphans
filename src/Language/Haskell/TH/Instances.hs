60aff125a649924f9851ac8cef6083d942a0a6d9
Michael Sloan <mgsloan@gmail.com>
2014-12-29 16:57:16 -0800

    Consolidate #ifdef for GHC7.10 support

--- src/Language/Haskell/TH/Instances.hs
+++ src/Language/Haskell/TH/Instances.hs
@@ -24,59 +24,41 @@ import Language.Haskell.TH.Lift (deriveLiftMany)
 import Language.Haskell.TH.ReifyMany
 import Language.Haskell.TH.Syntax
 
+-- Thanks to Richard Eisenberg, GHC 7.10 adds many of the instances
+-- from this module.
 #if !MIN_VERSION_template_haskell(2,10,0)
 import GHC.Word (Word8)
 import Language.Haskell.TH.Ppr
-#endif
 
--- Orphan Show instances
-
-#if !MIN_VERSION_template_haskell(2,10,0)
 deriving instance Show Loc
-#endif
 
--- Orphan Eq instances
-
-#if !MIN_VERSION_template_haskell(2,10,0)
 deriving instance Eq Loc
 deriving instance Eq Info
-#endif
 
-#if MIN_VERSION_template_haskell(2,5,0) && !(MIN_VERSION_template_haskell(2,7,0))
-deriving instance Eq ClassInstance
-#endif
-
--- Orphan Ord instances
-
-#if !MIN_VERSION_template_haskell(2,10,0)
 instance Ord FixityDirection where
   (<=) InfixL _      = True
   (<=) _      InfixR = True
   (<=) InfixN InfixN = True
   (<=) _      _      = False
-#endif
 
-$(reifyManyWithoutInstances ''Ord [''Info] (`notElem` [''Ratio]) >>=
-  mapM deriveOrd)
-
--- Orphan Ppr instances
-
-#if !MIN_VERSION_template_haskell(2,10,0)
 -- TODO: make this better
 instance Ppr Loc where
   ppr = showtextl . show
 
 instance Ppr Lit where
   ppr l = ppr (LitE l)
-#endif
 
--- Orphan Lift instances (for when your TH generates TH!)
-
-#if !MIN_VERSION_template_haskell(2,10,0)
 -- This follows the pattern of the Lift instances for Int / Integer.
 instance Lift Word8 where
   lift w = [e| fromIntegral $(lift (fromIntegral w :: Int)) |]
 #endif
+
+#if MIN_VERSION_template_haskell(2,5,0) && !(MIN_VERSION_template_haskell(2,7,0))
+deriving instance Eq ClassInstance
+#endif
+
+$(reifyManyWithoutInstances ''Ord [''Info] (`notElem` [''Ratio]) >>=
+  mapM deriveOrd)
 
 $(reifyManyWithoutInstances ''Lift [''Info] (const True) >>=
   deriveLiftMany)
