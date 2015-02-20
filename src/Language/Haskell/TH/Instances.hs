5445db60b1ec63000687b07a80c60a56206598b3
Michael Sloan <mgsloan@gmail.com>
2015-02-20 14:13:12 -0800

    Don't use TH to define Ord instances #14

--- src/Language/Haskell/TH/Instances.hs
+++ src/Language/Haskell/TH/Instances.hs
@@ -23,7 +23,6 @@ module Language.Haskell.TH.Instances () where
 
 import GHC.Real (Ratio)
 import Language.Haskell.TH
-import Language.Haskell.TH.Instances.Internal
 import Language.Haskell.TH.Lift (deriveLiftMany)
 import Language.Haskell.TH.ReifyMany
 import Language.Haskell.TH.Syntax
@@ -57,12 +56,66 @@ instance Lift Word8 where
   lift w = [e| fromIntegral $(lift (fromIntegral w :: Int)) |]
 #endif
 
-#if MIN_VERSION_template_haskell(2,5,0) && !(MIN_VERSION_template_haskell(2,7,0))
-deriving instance Eq ClassInstance
-#endif
-
-$(reifyManyWithoutInstances ''Ord [''Info] (`notElem` [''Ratio]) >>=
-  mapM deriveOrd)
-
 $(reifyManyWithoutInstances ''Lift [''Info, ''Loc] (const True) >>=
   deriveLiftMany)
+
+-- Ideally, it'd be possible to use reifyManyWithoutInstances for
+-- these Ord instances, but TH can't output deriving instances (and
+-- even if this is added for later versions, we need to support many
+-- ghc / th versions).  We can't generate Ord instances from TH due to
+-- some undiagnosed funkiness:
+-- https://github.com/mgsloan/th-orphans/issues/14
+
+-- GHC 7.10 comes with Ord instances for TH datatypes.
+#if !MIN_VERSION_template_haskell(2,10,0)
+deriving instance Ord Info
+deriving instance Ord Fixity
+deriving instance Ord Exp
+deriving instance Ord Dec
+deriving instance Ord Stmt
+deriving instance Ord Type
+deriving instance Ord Foreign
+deriving instance Ord FunDep
+deriving instance Ord Con
+deriving instance Ord Body
+deriving instance Ord Clause
+deriving instance Ord Strict
+deriving instance Ord Safety
+deriving instance Ord Callconv
+deriving instance Ord Guard
+deriving instance Ord Range
+deriving instance Ord Match
+deriving instance Ord Pat
+deriving instance Ord Lit
+
+#if MIN_VERSION_template_haskell(2,4,0)
+deriving instance Ord FamFlavour
+deriving instance Ord Pragma
+deriving instance Ord Pred
+deriving instance Ord TyVarBndr
+#endif
+
+#if MIN_VERSION_template_haskell(2,4,0) && !(MIN_VERSION_template_haskell(2,8,0))
+deriving instance Ord InlineSpec
+deriving instance Ord Kind
+#endif
+
+#if MIN_VERSION_template_haskell(2,5,0) && !(MIN_VERSION_template_haskell(2,7,0))
+deriving instance Eq ClassInstance
+deriving instance Ord ClassInstance
+#endif
+
+#if MIN_VERSION_template_haskell(2,8,0)
+deriving instance Ord Inline
+deriving instance Ord Phases
+deriving instance Ord RuleBndr
+deriving instance Ord RuleMatch
+deriving instance Ord TyLit
+#endif
+
+#if MIN_VERSION_template_haskell(2,9,0)
+deriving instance Ord AnnTarget
+deriving instance Ord Role
+deriving instance Ord TySynEqn
+#endif
+#endif
